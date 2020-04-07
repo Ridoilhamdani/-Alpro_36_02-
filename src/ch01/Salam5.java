d78b91fa8036bf98ebe36628c51a2a36ea6311e7
aldiansyahpramudia <aldiansyahaldi32@gmail.com>
2020-04-07 20:29:55 +0700

    Commit pertama

---
+++ src/ch01/Salam5.java
@@ -0,0 +1,9 @@
+package ch01;
+
+public class Salam5 {
+    public static void main(String[] args){
+	String nama = "Citra";
+	String kalimat = "hai " + nama;
+	System.out.println(kalimat);
+    }
+}
---
+++ src/ch02/Lingkaran.java
@@ -0,0 +1,8 @@
+package ch02;
+
+public class Lingkaran {
+    double jariJari;
+    double luas() {
+	return Math.PI * jariJari * jariJari;
+    }
+}
